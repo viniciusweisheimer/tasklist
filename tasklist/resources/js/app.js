89aeab8746421b1ee49e43a70da572d3b2f02b55
Vinicius Weisheimer <viniciusmw7@gmail.com>
2018-12-10 17:59:56 -0200

    Adicionado o tasklist ao app.js

--- tasklist/resources/js/app.js
+++ tasklist/resources/js/app.js
@@ -31,3 +31,5 @@ Vue.component('example-component', require('./components/ExampleComponent.vue'))
 const app = new Vue({
     el: '#app'
 });
+
+Vue.component('task-list', require('./components/TaskList.vue'));
